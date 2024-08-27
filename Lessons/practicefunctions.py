4254b8209ebaa2f8661259f9ea7540969c636a7a
Katherine Durham <duekat@ad.unc.edu>
2024-08-26 20:50:42 -0400

    EX00 Finished

---
+++ Lessons/practicefunctions.py
@@ -0,0 +1,15 @@
+# from random import randint
+
+# print(randint(1, 100))
+# comment them out, can undo later
+
+
+# function Definition example (making notes to yourself- just click hashtag)
+def sum(num1: int, num2: int) -> int:
+    """Return the sum of num1 and num2"""
+    return num1 + num2
+
+
+# Function Call
+sum(num1=2, num2=13)
+print(sum(num1=2, num2=13))  # <- these are arguments
